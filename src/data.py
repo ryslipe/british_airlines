b82903ca38e5343cbae11b8e728c2146e41f329f
Ryan Slipe <rslipe@my.gcu.edu>
2025-01-29 15:29:02 -0700

    trying to fix PYTHONPATH issues

---
+++ src/data.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import sys
+from pathlib import Path
+import sys
+import os
+print("PYTHONPATH:", os.environ.get('PYTHONPATH'))
+
+from src.paths import RAW_DATA_DIR
+
+
