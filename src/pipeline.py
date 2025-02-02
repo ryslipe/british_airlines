a07057081b66b6eb685266dc13954b8faaff9966
Ryan Slipe <rslipe@my.gcu.edu>
2025-02-02 15:32:07 -0700

    chi2 equations

--- src/pipeline.py
+++ src/pipeline.py
@@ -3,18 +3,21 @@ from sklearn.pipeline import make_pipeline, Pipeline
 from sklearn.linear_model import SGDClassifier
 from sklearn.feature_selection import SelectPercentile, chi2
 
-# create pipeline
+# Define the pipeline creation function
 def get_pipeline(percentile: int, **hyperparams) -> Pipeline:
     '''Pipeline we use for our model.'''
-    # print statement
+    
+    # Print statement for initializing pipeline 
     print("Creating pipeline with hyperparameters:", hyperparams)
-    # print statement for feature selection
+
+    # Print statement for the current percent of features selected using chi2
     print(f'Selecting best {percentile} percent of features.')
-    # create pipeline
+
+    # Create pipeline - first use feature selection then fit SGDCLassifier with optimized hyperparams
     selector = SelectPercentile(chi2, percentile=percentile)
-    pipeline = make_pipeline(
-        selector,
-        SGDClassifier(**hyperparams)
-    )
-    print("Pipeline created:")
+    pipeline = Pipeline([
+        ('selector', selector),
+        ('classifier', SGDClassifier(**hyperparams))
+    ])
+    print("Pipeline created.")
     return pipeline
